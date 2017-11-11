42688cef0988489ee71929ed927b2ceaaad01982
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-11 15:03:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue (object, key, value) {
-  return Object.assign({}, object, key, value);
+  return Object.assign({}, object, key, value, value);
 }
